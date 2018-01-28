95d7c6739fc559ca73debc456113027c856e82a7
Charlie Kassel <charlie.kassel@vice.com>
2018-01-28 14:03:27 +0000

    start working on array change diff - added basic structure

---
+++ src/ArrayDiff.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Differ;
+
+/**
+ * Class to find changes between two arrays
+ *
+ * Outputs an associative array with added, removed and changed keys
+ *
+ */
+class ArrayDiff
+{
+    /**
+     * @param array $old
+     * @param array $new
+     * @return array
+     */
+    public function diff(array $old, array $new): array
+    {
+        $added = $this->findAddedKeys($old, $new);
+        $removed = $this->findRemovedKeys($old, $new);
+        $changed = $this->findChangedKeys($old, $new);
+
+        return compact('added', 'removed', 'changed');
+    }
+
+    /**
+     * @param array $old
+     * @param array $new
+     * @return array
+     */
+    private function findAddedKeys(array $old, array $new): array
+    {
+        return array_filter($new, function ($key) use ($old) {
+            return !array_key_exists($key, $old);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+
+    /**
+     * @param array $old
+     * @param array $new
+     * @return array
+     */
+    private function findRemovedKeys(array $old, array $new): array
+    {
+        return array_filter($old, function ($key) use ($new) {
+            return !array_key_exists($key, $new);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+
+    /**
+     * @param array $old
+     * @param array $new
+     * @return array
+     */
+    private function findChangedKeys(array $old, array $new): array
+    {
+        $changed = array_filter($new, function ($newItem, $key) use ($old) {
+            return array_key_exists($key, $old) && $old[$key] !== $newItem;
+        }, ARRAY_FILTER_USE_BOTH);
+
+        array_walk($changed, function (&$changedItem, $key) use ($old) {
+            if (is_array($changedItem)) {
+                $changedItem = $this->diff($old[$key], $changedItem);
+            }
+        });
+
+        return $changed;
+    }
+}
---
+++ tests/ArrayDiffTest.php
@@ -0,0 +1,126 @@
+<?php
+
+use Differ\ArrayDiff;
+use PHPUnit\Framework\TestCase;
+
+class ArrayDiffTest extends TestCase
+{
+    public function testDiffReturnsAddedRemovedChangedKeys()
+    {
+        $differ = new ArrayDiff();
+        $diff = $differ->diff(['a', 'b', 'c'], ['b', 'c', 'd']);
+
+        $this->assertArrayHasKey('added', $diff);
+        $this->assertArrayHasKey('removed', $diff);
+        $this->assertArrayHasKey('changed', $diff);
+    }
+
+    /**
+     * @dataProvider addedKeyProvider
+     */
+    public function testFindsAddedKeys($old, $new, $expectedKey)
+    {
+        $differ = new ArrayDiff();
+        $diff = $differ->diff($old, $new);
+
+        $this->assertArrayHasKey($expectedKey, $diff['added']);
+    }
+
+    public function addedKeyProvider()
+    {
+        return [
+            [['a', 'b', 'c'], ['a', 'b', 'c', 'd'], 3],
+            [['a' => 1, 'b' => 2, 'c' => 3], ['b' => 2, 'c' => 3, 'd' => 4], 'd'],
+            [['a' => 1, 'b' => 2, 'c' => 3], ['b' => 2, 'c' => 3, 'd' => null], 'd'],
+            [['a' => 1], ['b' => 2, 'c' => 3, 'd' => null], 'b'],
+        ];
+    }
+
+    /**
+     * @dataProvider nestedAddedKeyProvider
+     */
+    public function testFindsNestedAddedKeys($old, $new, $expectedKey)
+    {
+        $differ = new ArrayDiff();
+        $diff = $differ->diff($old, $new);
+
+        $this->assertArrayHasKey($expectedKey, $diff['changed']['a']['added']);
+    }
+
+    public function nestedAddedKeyProvider()
+    {
+        return [
+            // test nested added numeric key
+            [['a' => [1, 2]], ['a' => [1, 2, 3]], 2],
+            [['a' => ['one', 'two', 'three']], ['a' => ['one', 'two', 'three', 'four']], 3],
+            // test nested added associative key
+            [['a' => ['one' => 1, 'two' => 1, 'three' => 1]], ['a' => ['one' => 1, 'two' => 1, 'three' => 1, 'four' => []]], 'four'],
+        ];
+    }
+
+    /**
+     * @dataProvider removedKeyProvider
+     */
+    public function testFindsRemovedKeys($old, $new, $expectedKey)
+    {
+        $differ = new ArrayDiff();
+        $diff = $differ->diff($old, $new);
+
+        $this->assertArrayHasKey($expectedKey, $diff['removed']);
+    }
+
+    public function removedKeyProvider()
+    {
+        return [
+            [['a', 'b', 'c', 'd'], ['a', 'b', 'c'], 3],
+            [['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], ['b' => 2, 'c' => 3], 'd'],
+            [['a' => 1, 'b' => 2, 'c' => 3, 'd' => null], ['b' => 2, 'c' => 3], 'd'],
+            [['a' => 1, 'b' => 2, 'c' => 3, 'd' => null], ['b' => 2, 'c' => 3], 'a'],
+            [['b' => 2, 'c' => 3, 'd' => null], ['a' => 1], 'b'],
+            [['c' => 3, 'd' => null], ['a' => 1, 'b' => 2], 'c'],
+        ];
+    }
+
+    /**
+     * @dataProvider nestedRemovedKeyProvider
+     */
+    public function testFindsNestedRemovedKeys($old, $new, $expectedKey)
+    {
+        $differ = new ArrayDiff();
+        $diff = $differ->diff($old, $new);
+
+        $this->assertArrayHasKey($expectedKey, $diff['changed']['a']['removed']);
+    }
+
+    public function nestedRemovedKeyProvider()
+    {
+        return [
+            // test nested removed numeric key
+            [['a' => [1, 2, 3]], ['a' => [1, 2]], 2],
+            [['a' => ['one', 'two', 'three', 'four']], ['a' => ['one', 'two', 'three']], 3],
+            // test nested removed associative key
+            [['a' => ['one' => 1, 'two' => 1, 'three' => 1, 'four' => []]], ['a' => ['one' => 1, 'two' => 1, 'three' => 1]], 'four'],
+        ];
+    }
+
+    /**
+     * @dataProvider changedKeyProvider
+     */
+    public function testFindsChangedKeys($old, $new, $expectedKey)
+    {
+        $differ = new ArrayDiff();
+        $diff = $differ->diff($old, $new);
+
+        $this->assertArrayHasKey($expectedKey, $diff['changed']);
+    }
+
+    public function changedKeyProvider()
+    {
+        return [
+            [['a', 'b', 'd'], ['a', 'b', 'c'], 2],
+            [['b' => 2, 'c' => 3], ['b' => 2, 'c' => 4], 'c'],
+            [['b' => 2, 'c' => 3, 'd' => null], ['b' => 2, 'c' => 3, 'd' => 1], 'd'],
+            [['b' => 2, 'c' => 3, 'd' => null], ['b' => 2, 'c' => 5], 'c'],
+        ];
+    }
+}
